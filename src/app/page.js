2c8406f126e24156664bd01cd702b4d543fd0e63
Ronny Haase <ronnyhaase@fastmail.com>
2023-06-14 00:10:14 +0200

    Make metadata static, use full URL

--- src/app/page.js
+++ src/app/page.js
@@ -1,29 +1,27 @@
 import { SolFeesApp } from "@/components/SolFeesApp";
 
-export async function generateMetadata() {
-  return {
-    metadataBase: new URL('https://solfees.fyi'),
+export const metadata = {
+  metadataBase: new URL('https://www.solfees.fyi'),
+  title: 'solfees.fyi',
+  description: 'Check how much a Solana wallet has spent on fees',
+  openGraph: {
     title: 'solfees.fyi',
     description: 'Check how much a Solana wallet has spent on fees',
-    openGraph: {
-      title: 'solfees.fyi',
-      description: 'Check how much a Solana wallet has spent on fees',
-      url: '/',
-      siteName: 'solfees.fyi',
-      images: [{
-        url: '/solfeesfyi.jpeg',
-        width: 700,
-        height: 350,
-      }],
-    },
-    twitter: {
-      card: 'summary_large_image',
-      title: 'solfees.fyi',
-      description: 'Check how much a Solana wallet has spent on fees',
-      creator: '@ronnyhaase',
-      images: ['/solfeesfyi.jpeg'],
-    },
-  }
+    url: '/',
+    siteName: 'solfees.fyi',
+    images: [{
+      url: 'https://www.solfees.fyi/solfeesfyi.jpeg',
+      width: 700,
+      height: 350,
+    }],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'solfees.fyi',
+    description: 'Check how much a Solana wallet has spent on fees',
+    creator: '@ronnyhaase',
+    images: ['/https://www.solfees.fyi/solfeesfyi.jpeg'],
+  },
 }
 
 export default function Home() {
