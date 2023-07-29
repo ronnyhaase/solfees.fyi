98f2ce7d035dca7cb0e39a77b8b749ec65884b3d
Ronny Haase <ronnyhaase@fastmail.com>
2023-07-29 18:23:21 +0200

    Add API test script

--- src/utils.js
+++ src/utils.js
@@ -2,7 +2,7 @@ const SOL_PER_LAMPORT = 0.000000001
 
 const isSolanaAddress = value => /^[1-9A-HJ-NP-Za-km-z]{32,44}$/.test(value)
 
-const isSolanaDomain = value => /^.+.(abc|backpack|bonk|poor|glow|sol)$/i.test(value)
+const isSolanaDomain = value => /^.+\.(abc|backpack|bonk|poor|glow|sol)$/i.test(value)
 
 export {
   SOL_PER_LAMPORT,
