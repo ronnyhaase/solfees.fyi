ae22d006a0f991ae2412caf01dcc00e8f0a3ae63
Ronny Haase <ronnyhaase@fastmail.com>
2023-10-06 18:39:04 +0200

    Add Birdeye API key

--- src/hooks/useSolPrice.js
+++ src/hooks/useSolPrice.js
@@ -7,7 +7,10 @@ function useSolPrice() {
 
   useEffect(() => {
     setIsLoading(true)
-    ky.get('https://public-api.birdeye.so/public/price?address=So11111111111111111111111111111111111111112')
+    ky.get(
+      'https://public-api.birdeye.so/public/price?address=So11111111111111111111111111111111111111112',
+      { headers: { 'X-API-KEY': process.env.NEXT_PUBLIC_BIRDEYE_KEY } },
+    )
       .json()
       .then(res => setPrice(res.data.value))
       .then(() => setIsLoading(false))
